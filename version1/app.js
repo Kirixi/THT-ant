f5573406471074106e2a95c4f7c9df659374a1c9
Jonathan <70047005+Kirixi@users.noreply.github.com>
2024-11-01 15:23:19 +0800

    Update app.js

--- version1/app.js
+++ version1/app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-	res.status(200).send("hello world! new content");
+	res.status(200).send("hello world! new content asd");
 });
 
 app.listen(port, () => {
