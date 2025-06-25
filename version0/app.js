94a8d738f0c0bcfab431f8cee288fb7936921854
JonathanTeh <yeuhaw.teh@razer.com>
2025-06-25 22:40:40 +0800

    demo

--- version0/app.js
+++ version0/app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-	res.status(200).send("hello world! this is your second containerized app");
+	res.status(200).send("hello world! this is your first containerized app");
 });
 
 app.listen(port, () => {
